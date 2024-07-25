dccc76699511cde76f8cfe0b6fc5e0e5a6addb21
kuenpongfa221 <43448852+kuenpongfa221@users.noreply.github.com>
2024-07-25 16:33:09 +0800

    測試假bootstrap的row col功能以及card功能

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+    <!-- <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    /> -->
+    <link rel="stylesheet" href="./styles/bootstrap5Fake.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
@@ -33,9 +41,32 @@
 
   <!-- Main -->
   <main class="et-main">
-    <section class="et-slide" id="tab-es6">
-      <h1>ES6</h1>
-      <h3>something about es6</h3>
+    <section class="et-slide container-fluid" id="tab-es6">
+      <!-- <h1>ES6</h1>
+      <h3>something about es6</h3> -->
+      <!-- <div class="container-fluid"> -->
+        <div class="row">
+          <div class="col-6">123</div>
+          <div class="col-6">456</div>
+        </div>
+        <div class="row">
+          <div class="col-4">  
+            <h2>Card Image</h2>
+            <p>Image at the top (card-img-top):</p>
+            <div class="card" style="width:400px">
+              <img class="card-img-top" src="../bootstrap4/img_avatar1.png" alt="Card image" style="width:100%">
+              <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-4">456</div>
+          <div class="col-4">789</div>
+        </div>
+      <!-- </div> -->
     </section>
     <section class="et-slide" id="tab-flexbox">
       <h1>Flexbox</h1>
@@ -52,6 +83,7 @@
 
   </main>
 
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
   <script src="./script/stickyNavigation.js"></script>
 </body>
 </html>
