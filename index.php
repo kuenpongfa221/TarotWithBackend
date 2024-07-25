d1cf94a36d38d9c1c8bd0d735ad66a7909db2bb2
kuenpongfa221 <43448852+kuenpongfa221@users.noreply.github.com>
2024-07-25 20:40:44 +0800

    完成「客戶回饋」section

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap" rel="stylesheet">
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
     <!-- <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
@@ -25,14 +29,14 @@
       </a>
      </div>
     <div class="et-hero-tabs-container">
-      <a class="et-hero-tab" href="#tab-home">預約占卜</a>
-      <a class="et-hero-tab" href="#tab-es6">牌義 • 教學</a>
-      <a class="et-hero-tab" href="#tab-flexbox">客戶回饋</a>
-      <a class="et-hero-tab" href="#tab-react">商品</a>
-      <a class="et-hero-tab" href="#tab-angular">服務</a>
+      <a class="et-hero-tab" href="#tab-reserve">預約占卜</a>
+      <a class="et-hero-tab" href="#tab-card-meaning">牌義 • 教學</a>
+      <a class="et-hero-tab" href="#tab-testimonials">客戶回饋</a>
+      <a class="et-hero-tab" href="#tab-product">商品</a>
+      <a class="et-hero-tab" href="#tab-service">服務</a>
       <span class="et-hero-tab-slider"></span>
     </div>
-    <div class="img-container" id="tab-home">
+    <div class="img-container" id="tab-reserve">
       <h2>塔羅，遇見你的內在天賦之旅</h2>
       <!-- <img src="./images/tarotHeroSec.jpg" alt=""> -->
        <button id="reserveBtn">立即預約占卜</button>
@@ -41,7 +45,7 @@
 
   <!-- Main -->
   <main class="et-main">
-    <section class="et-slide container-fluid" id="tab-es6">
+    <section class="et-slide container-fluid" id="tab-card-meaning">
       <!-- <h1>ES6</h1>
       <h3>something about es6</h3> -->
       <!-- <div class="container-fluid"> -->
@@ -88,15 +92,151 @@
         </div>
       <!-- </div> -->
     </section>
-    <section class="et-slide" id="tab-flexbox">
-      <h1>Flexbox</h1>
-      <h3>something about flexbox</h3>
+    <section class="et-slide" id="tab-testimonials">
+      <h1>客戶回饋</h1>
+      <div class="row gy-5 gx-4">
+          <div
+            class="col-lg-4 col-sm-6"
+            data-aos="fade-down"
+            data-aos-delay="150"
+          >
+            <div class="review">
+              <div class="review-head p-4 bg-white theme-shadow">
+                <div class="text-warning">
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                </div>
+                <p>
+                  Amazing theme ipsum dolor sit amet consectetur adipisicing
+                  elit. Assumenda eum animi rerum ipsam impedit dicta
+                  voluptatem.
+                </p>
+              </div>
+              <div class="review-person mt-4 d-flex align-items-center">
+                <img
+                  class="rounded-circle"
+                  src="./images/avatar-1.jpg"
+                  alt=""
+                />
+                <div class="  ">
+                  <h5>Dianne Russell</h5>
+                  <small>UX Architect</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-lg-4 col-sm-6"
+            data-aos="fade-down"
+            data-aos-delay="150"
+          >
+            <div class="review">
+              <div class="review-head p-4 bg-white theme-shadow">
+                <div class="text-warning">
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                </div>
+                <p>
+                  Amazing theme ipsum dolor sit amet consectetur adipisicing
+                  elit. Assumenda eum animi rerum ipsam impedit dicta
+                  voluptatem.
+                </p>
+              </div>
+              <div class="review-person mt-4 d-flex align-items-center">
+                <img
+                  class="rounded-circle"
+                  src="./images/avatar-1.jpg"
+                  alt=""
+                />
+                <div class="  ">
+                  <h5>Dianne Russell</h5>
+                  <small>UX Architect</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-lg-4 col-sm-6"
+            data-aos="fade-down"
+            data-aos-delay="150"
+          >
+            <div class="review">
+              <div class="review-head p-4 bg-white theme-shadow">
+                <div class="text-warning">
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                </div>
+                <p>
+                  Amazing theme ipsum dolor sit amet consectetur adipisicing
+                  elit. Assumenda eum animi rerum ipsam impedit dicta
+                  voluptatem.
+                </p>
+              </div>
+              <div class="review-person mt-4 d-flex align-items-center">
+                <img
+                  class="rounded-circle"
+                  src="./images/avatar-1.jpg"
+                  alt=""
+                />
+                <div class="  ">
+                  <h5>Dianne Russell</h5>
+                  <small>UX Architect</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-lg-4 col-sm-6"
+            data-aos="fade-down"
+            data-aos-delay="150"
+          >
+            <div class="review">
+              <div class="review-head p-4 bg-white theme-shadow">
+                <div class="text-warning">
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                  <i class="ri-star-fill"></i>
+                </div>
+                <p>
+                  Amazing theme ipsum dolor sit amet consectetur adipisicing
+                  elit. Assumenda eum animi rerum ipsam impedit dicta
+                  voluptatem.
+                </p>
+              </div>
+              <div class="review-person mt-4 d-flex align-items-center">
+                <img
+                  class="rounded-circle"
+                  src="./images/avatar-1.jpg"
+                  alt=""
+                />
+                <div class="  ">
+                  <h5>Dianne Russell</h5>
+                  <small>UX Architect</small>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
     </section>
-    <section class="et-slide" id="tab-react">
+    <section class="et-slide" id="tab-product">
       <h1>React</h1>
       <h3>something about react</h3>
     </section>
-    <section class="et-slide" id="tab-angular">
+    <section class="et-slide" id="tab-service">
       <h1>Angular</h1>
       <h3>something about angular</h3>
     </section>
